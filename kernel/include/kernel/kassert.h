5199795c3502df7a9e6cd15cc90955639b3ee49a
Isak Evaldsson <isak.evaldsson@outlook.com>
2023-01-10 21:09:03 +0100

    Kernel assert macro

---
+++ kernel/include/kernel/kassert.h
@@ -0,0 +1,11 @@
+#ifndef K_ASSERT_H
+#define K_ASSERT_H
+
+// Kernel equivalent of c-macro
+#define kassert(expr)                                                                 \
+    if (!(expr))                                                                      \
+    {                                                                                 \
+        printf("Kernel assertion '%s' failed at %s:%u\n", #expr, __FILE__, __LINE__); \
+        abort();                                                                      \
+    }
+#endif
