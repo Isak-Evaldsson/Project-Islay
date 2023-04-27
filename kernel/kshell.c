499c549f9001709de4bb8ed0fed41457c721c490
Isak <isak.evaldsson@outlook.com>
2023-04-27 16:03:50 +0200

    kshell: better cmd parsing and clear cmd

--- kernel/kshell.c
+++ kernel/kshell.c
@@ -6,6 +6,20 @@
     Basic kernel shell
 */
 
+// Internal helper function
+static void print_help();
+
+typedef struct command_t {
+    const char *name;
+    const char *description;
+    void (*function)();
+} command_t;
+
+static command_t commands[] = {
+    (command_t){.name = "help",  .description = "prints all available shell commands", print_help},
+    (command_t){.name = "clear", .description = "clears the terminal window",          term_clear},
+};
+
 static void print_kernel_header()
 {
     kprintf("Project Islay, version 0.0.1 (pre-alpha)\n");
@@ -15,17 +29,22 @@ static void print_kernel_header()
 static void print_help()
 {
     kprintf("Available commands:\n");
-    kprintf("   help: prints all available shell commands\n");
+    for (size_t i = 0; i < sizeof(commands) / sizeof(commands[0]); i++)
+        kprintf("   %s: %s\n", commands[i].name, commands[i].description);
 }
 
-static void parse_command(const char* cmd)
+static void parse_command(const char *cmd)
 {
-    if (strcmp(cmd, "help") == 0) {
-        print_help();
-        return;
+    for (size_t i = 0; i < sizeof(commands) / sizeof(commands[0]); i++) {
+        command_t command = commands[i];
+
+        if (strcmp(cmd, command.name) == 0) {
+            command.function();
+            return;
+        }
     }
 
-    kprintf("Invalid command: %s\n", cmd);
+    if (strcmp(cmd, "clear")) kprintf("Invalid command: %s\n", cmd);
 }
 
 void kshell()
