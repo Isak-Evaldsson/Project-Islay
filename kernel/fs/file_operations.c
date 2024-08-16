e4a0f8383871fc58134859ff26e4b1aba4235c7a
Isak Evaldsson <isak.evaldsson@outlook.com>
2024-08-16 10:57:13 +0200

    fs: Implement read and pread
    Implementing the vfs part of read and pread, which is resposible for
    finding the file based on the fd and calling the fs implemenation's read
    function.

--- kernel/fs/file_operations.c
+++ kernel/fs/file_operations.c
@@ -62,3 +62,44 @@ int close(struct task_fs_data* task_data, int fd)
     return 0;
 }
 
+static ssize_t read_helper(struct task_fs_data* task_data, int fd, void* buf, size_t nbyte,
+                           off_t offset, bool use_file_offset)
+{
+    int               read_bytes;
+    off_t             read_offset;
+    struct open_file* file;
+
+    if (fd < 0 || fd >= MAX_OPEN_PER_PROC) {
+        return -EBADF;
+    }
+
+    file = task_data->file_table[fd];
+    if (!file) {
+        return -EBADF;
+    }
+
+    read_offset = use_file_offset ? file->offset : offset;
+
+    // TODO: Should we count the offset/bytes to ensure that we don't over-read or should we hand
+    // over the responsibilty to the fs implementation?
+    read_bytes = file->file_ops->read(buf, nbyte, file->offset, file);
+    if (read_bytes < 0) {
+        return read_bytes;
+    }
+
+    if (use_file_offset) {
+        file->offset += read_bytes;
+    }
+
+    return read_bytes;
+}
+
+ssize_t pread(struct task_fs_data* task_data, int fd, void* buf, size_t nbyte, off_t offset)
+{
+    return read_helper(task_data, fd, buf, nbyte, offset, false);
+}
+
+ssize_t read(struct task_fs_data* task_data, int fd, void* buf, size_t nbyte)
+{
+    return read_helper(task_data, fd, buf, nbyte, 0, true);
+}
