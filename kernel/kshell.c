6696126e265c8e5999e0ab9d56f3b9bb7dec138e
Isak Evaldsson <isak.evaldsson@outlook.com>
2024-08-16 11:15:20 +0200

    kshell: Implement sysread command
    The sysread command, as the name suggests, reads from files within
    sysfs. The purpose of this command is to enable fs debugging.

--- kernel/kshell.c
+++ kernel/kshell.c
@@ -1,30 +1,35 @@
+#include <arch/paging.h>
 #include <arch/tty.h>
 #include <devices/ps2_keyboard.h>
 #include <fs.h>
 #include <memory/page_frame_manager.h>
+#include <memory/vmem_manager.h>
 #include <stdbool.h>
+#include <tasks/scheduler.h>
 #include <utils.h>
 
 /*
     Basic kernel shell
 */
-void dump_vfs();
 
-/* Internal helper function */
+/* Internal helper functions */
 static void print_help();
 static void mem_stats();
+static void read_cmd(char *arg);
 
 typedef struct command_t {
     const char *name;
     const char *description;
-    void (*function)();
+    void (*function)(char *arg);
 } command_t;
+
+static char *buf;
 
 static command_t commands[] = {
     {.name = "help",    .description = "prints all available shell commands", print_help},
     {.name = "clear",   .description = "clears the terminal window",          term_clear},
     {.name = "memstat", .description = "show kernel memory statistics",       mem_stats },
-    {.name = "dumpfs",  .description = "vfs statists",                        dump_vfs  }
+    {.name = "sysread", .description = "read file from sysfs",                read_cmd  },
 };
 
 static void print_kernel_header()
@@ -49,13 +54,45 @@ static void mem_stats()
     kprintf("%u of %u available page frames\n", mem.n_available_frames, mem.n_frames);
 }
 
+static void read_cmd(char *arg)
+{
+    char path[100];
+
+    snprintf(path, sizeof(path), "/sys/%s", arg);
+    int fd = open(&scheduler_get_current_task()->fs_data, path);
+    if (fd < 0) {
+        kprintf("failed to open %s, errno -%u\n", path, -fd);
+        return;
+    }
+
+    ssize_t nbytes = read(&scheduler_get_current_task()->fs_data, fd, buf, PAGE_SIZE - 1);
+    if (nbytes < 0) {
+        kprintf("Failed to read fd, errno -%u\n", fd, -nbytes);
+        goto end;
+    }
+
+    // Ensure that we print a null-terminated string
+    buf[nbytes] = '\0';
+    kprintf("%s", buf);
+
+end:
+    int res = close(&scheduler_get_current_task()->fs_data, fd);
+    if (res < 0) {
+        kprintf("failed to close fd: %u, errno -%u", fd, -res);
+    }
+}
+
 static void parse_command(const char *cmd)
 {
+    char *save_ptr;
+    char *name = strtok(cmd, " ", &save_ptr);
+    char *arg  = strtok(NULL, " ", &save_ptr);
+
     for (size_t i = 0; i < sizeof(commands) / sizeof(commands[0]); i++) {
         command_t command = commands[i];
 
-        if (strcmp(cmd, command.name) == 0) {
-            command.function();
+        if (strcmp(name, command.name) == 0) {
+            command.function(arg);
             return;
         }
     }
@@ -69,6 +106,13 @@ void kshell()
     char cmd[200];
 
     print_kernel_header();
+
+    buf = vmem_request_free_page(0);
+    if (!buf) {
+        kprintf("Failed to allocate kshell buffer\n");
+        return;
+    }
+
     while (true) {
         kprintf("> ");
         kreadline(sizeof(cmd), cmd);
