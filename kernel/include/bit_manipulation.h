f45cabf505b905df55abd7ffd3a56daef8df86a9
Isak Evaldsson <isak.evaldsson@outlook.com>
2024-08-24 10:39:13 +0200

    bitmanipulation.h: add GET_LOWEST_BITS and MASK_LOWEST_BITS
    Additonal macros to make bitmasking more convenient.

--- kernel/include/bit_manipulation.h
+++ kernel/include/bit_manipulation.h
@@ -5,4 +5,7 @@
 #define CLR_BIT(b, n)  ((b) &= ~((1) << (n)))
 #define MASK_BIT(b, n) ((b) & (1 << n))
 
+#define GET_LOWEST_BITS(b, n)  ((b) & ((1 << (n)) - 1))
+#define MASK_LOWEST_BITS(b, n) ((b) & ~((1 << (n)) - 1))
+
 #endif /* KLIB_BIT_MANIPULATION_H */
