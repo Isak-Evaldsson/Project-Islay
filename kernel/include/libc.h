60fbac549898eda9771725e5b8c03bea10ca55ec
Isak Evaldsson <isak.evaldsson@outlook.com>
2024-08-25 19:22:00 +0200

    libc: Implement strnlen

--- kernel/include/libc.h
+++ kernel/include/libc.h
@@ -15,6 +15,7 @@ void  *memcpy(void *__restrict dest, const void *__restrict src, size_t count);
 void  *memmove(void *dest, const void *src, size_t count);
 void  *memset(void *dest, int ch, size_t count);
 size_t strlen(const char *str);
+size_t strnlen(const char *str, size_t count);
 int    strcmp(const char *lhs, const char *rhs);
 int    strncmp(const char *lhs, const char *rhs, size_t count);
 char  *strdup(const char *str1);
