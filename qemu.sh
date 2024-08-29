93faa27b6515f913a5064119ee17b414f02c45b4
Isak Evaldsson <isak.evaldsson@outlook.com>
2024-08-29 19:45:21 +0200

    qemu.sh: Add a gdb option the qemu.sh
    With the introduction of an initrd using the makefile in ./kernel
    becomes a bit troublesome sine one want to build the initrd as well, the
    bash script works better but lacked the gdb option from the kernel
    makefile.

--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 set -e
-. ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -action reboot=shutdown -d int -action shutdown=pause -cdrom islayos.iso
+GDB=false
+BUILD="kernel/build"
+
+QEMU_OPTS=(
+    -cdrom islayos.iso
+    -no-shutdown
+    -no-reboot
+    -d int
+    -D $BUILD/log.txt
+    -serial file:$BUILD/serial
+)
+
+if [ $# -ge 1 ]; then
+    if [ $1 = "gdb" ]; then
+        GDB=true
+    else
+        echo "unknown option"
+        exit 1
+    fi
+fi
+
+source ./iso.sh
+
+if [ $GDB = true ]; then
+    qemu-system-$(./target-triplet-to-arch.sh $HOST) "${QEMU_OPTS[@]}" -S -s &
+    gdb "$BUILD/kernel.elf"  -ex 'target remote localhost:1234' -ex 'layout src' -ex 'layout regs' -ex 'break *_start' -ex 'continue'
+else
+    qemu-system-$(./target-triplet-to-arch.sh $HOST) "${QEMU_OPTS[@]}"
+fi
