74f8d270024827aa7591e6f6f7838acae6148ebf
Isak Evaldsson <isak.evaldsson@outlook.com>
2024-09-29 12:38:34 +0200

    x86-interrupts: Removed redundant header
    Removed kernel/arch/i386/interrupts/interrupts.h since it was only used
    by kernel/arch/i386/interrupts/interrupts.c, having another interrut
    wihtin the tree only makes things confusing.

--- kernel/arch/i386/interrupts/interrupts.c
+++ kernel/arch/i386/interrupts/interrupts.c
@@ -11,7 +11,7 @@
 
 #include "../drivers/pit.h"
 #include "../processor.h"
-#include "interrupts.h"
+#include "../segmentation/gdt.h"
 #include "pic.h"
 #include "ps2.h"
 
@@ -30,6 +30,65 @@ typedef struct interrupt_descriptor_t {
     Interrupt descriptor table
 */
 static interrupt_descriptor_t idt[256];
+
+/*
+    Assembly routine for properly loading the ldt registers (see load_idt.S)
+    Uses the same pointer formatting as the lgdt instruction
+*/
+extern void load_idt(gdt_ptr_t *ptr);
+
+/*
+    Assembly defined sub-routines, see interrupt_handlers.S.
+    Declarations need here in order to avoid compiler errors.
+*/
+void interrupt_handler_0();
+void interrupt_handler_1();
+void interrupt_handler_2();
+void interrupt_handler_3();
+void interrupt_handler_4();
+void interrupt_handler_5();
+void interrupt_handler_6();
+void interrupt_handler_7();
+void interrupt_handler_8();
+void interrupt_handler_9();
+void interrupt_handler_10();
+void interrupt_handler_11();
+void interrupt_handler_12();
+void interrupt_handler_13();
+void interrupt_handler_14();
+void interrupt_handler_15();
+void interrupt_handler_16();
+void interrupt_handler_17();
+void interrupt_handler_18();
+void interrupt_handler_19();
+void interrupt_handler_20();
+void interrupt_handler_21();
+void interrupt_handler_22();
+void interrupt_handler_23();
+void interrupt_handler_24();
+void interrupt_handler_25();
+void interrupt_handler_26();
+void interrupt_handler_27();
+void interrupt_handler_28();
+void interrupt_handler_29();
+void interrupt_handler_30();
+void interrupt_handler_31();
+void interrupt_handler_32();
+void interrupt_handler_33();
+void interrupt_handler_34();
+void interrupt_handler_35();
+void interrupt_handler_36();
+void interrupt_handler_37();
+void interrupt_handler_38();
+void interrupt_handler_39();
+void interrupt_handler_40();
+void interrupt_handler_41();
+void interrupt_handler_42();
+void interrupt_handler_43();
+void interrupt_handler_44();
+void interrupt_handler_45();
+void interrupt_handler_46();
+void interrupt_handler_47();
 
 static void set_interrupt_descriptor(uint8_t index, uint32_t isr_addr)
 {
