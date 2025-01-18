22632e49e37b5a7df26de350e58a8a86f0869b7f
Isak Evaldsson <isak.evaldsson@outlook.com>
2025-01-18 14:10:08 +0100

    bit_manipulation.h: Add macro for inverting specific bits
    Adds macro to invert bits.

--- kernel/include/bit_manipulation.h
+++ kernel/include/bit_manipulation.h
@@ -10,6 +10,7 @@
 #define SET_BIT(b, n)  ((b) |= (1 << (n)))
 #define CLR_BIT(b, n)  ((b) &= ~((1) << (n)))
 #define MASK_BIT(b, n) ((b) & (1 << n))
+#define INV_BIT(b, n)  ((b) ^= (1 << (n)))
 
 #define GET_LOWEST_BITS(b, n)  ((b) & ((1 << (n)) - 1))
 #define MASK_LOWEST_BITS(b, n) ((b) & ~((1 << (n)) - 1))
