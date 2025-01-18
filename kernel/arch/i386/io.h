1f795bbb38f07a99cbfc41ff0b9a02680c889eab
Isak Evaldsson <isak.evaldsson@outlook.com>
2025-01-18 13:53:50 +0100

    x86: io.h: Ensure calls to inb and outb to be correctly ordered
    Some device requires io commands to read/written in a particular order,
    by inserting memory barriers within the io functions, they are
    guaranteed to follow the program order.

--- kernel/arch/i386/io.h
+++ kernel/arch/i386/io.h
@@ -6,16 +6,31 @@
 */
 #ifndef ARCH_i386_IO_H
 #define ARCH_i386_IO_H
+#include <atomics.h>
 #include <stdint.h>
+
+/*
+    Assembly definitons
+*/
+void    asm_outb(uint16_t port, uint8_t data);
+uint8_t asm_inb(uint16_t port);
 
 /*
     Sends a byte of data to the specified port
 */
-void outb(uint16_t port, uint8_t data);
+#define outb(port, data)      \
+    ({                        \
+        mem_barrier_full();   \
+        asm_outb(port, data); \
+    })
 
 /*
     Reads a byte of data from the specified port
 */
-uint8_t inb(uint16_t port);
+#define inb(port)           \
+    ({                      \
+        mem_barrier_full(); \
+        asm_inb(port);      \
+    })
 
 #endif /* ARCH_i386_IO_H */
