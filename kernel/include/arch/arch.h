f8178a9f76175fc4e187bb10b0b27eb6a5dcfddf
Isak Evaldsson <isak.evaldsson@outlook.com>
2026-07-12 13:44:48 +0200

    paging.h: Use standard type for virt/physaddr_t
    Better to use standard toolchain defintions that having an per arch
    definition.

--- kernel/include/arch/arch.h
+++ kernel/include/arch/arch.h
@@ -7,6 +7,7 @@
 #ifndef ARCH_ARCH_H
 #define ARCH_ARCH_H
 #include <arch/platfrom.h>
+#include <stdint.h>
 
 /*
     Header including architecture specific defintions
@@ -16,14 +17,16 @@
 #define ENDIAN_LITTLE 2
 
 #if ARCH(i686)
-#include <stdint.h>
-typedef uint32_t virtaddr_t;
-typedef uint32_t physaddr_t;
-
 #define ARCH_ENDIANNESS ENDIAN_LITTLE
 #else
 #error "Unkown architecture"
 #endif
+
+#define VIRTADDR_MAX UINTPTR_MAX
+#define PHYSADDR_MAX UINTPTR_MAX
+
+typedef uintptr_t virtaddr_t;
+typedef uintptr_t physaddr_t;
 
 /* To initialise arch-specific static devices that can be expected to always be there (e.g.
  * interrupt controllers) */
